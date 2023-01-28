9a9cacdc01d2e64c2c256e3a427bd39b25eddd77
tejas-warake <tswarake15@gmail.com>
2023-01-28 23:06:16 +0530

    Started with learning android dev using react native

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Constants from 'expo-constants';
+import RepositoryList from './RepositoryList.jsx';
+import { Text, StyleSheet, View } from 'react-native';
+
+const Main = () => {
+  return (
+    <RepositoryList />
+  );
+};
+
+export default Main;
